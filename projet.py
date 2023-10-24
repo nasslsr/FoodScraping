3daf67e8714518b91fec128e594c1773838c624d
nass <nasslsr@macbook-pro-de-nass.home>
2023-10-24 19:37:23 +0200

    adding the project file

---
+++ projet.py
@@ -0,0 +1,14 @@
+import requests
+
+url = "https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe"
+
+querystring = {"query":"carbo"}
+
+headers = {
+	"X-RapidAPI-Key": "c2eea8a81fmsha2cba9d9c3840f2p195d04jsn76a0cf0025dd",
+	"X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com"
+}
+
+response = requests.get(url, headers=headers, params=querystring)
+
+print(response.json())
