5ada5f18251102bfca97d86f78c9c45729f78b94
nass <nasslsr@MacBook-Pro-de-nass.local>
2023-10-27 14:56:45 +0200

    voici tous les fichiers mise a jour

--- index.py
+++ index.py
@@ -1,11 +1,19 @@
 from flask import Flask, render_template, request
 import requests
 import re
+import requests
+import re
+from selenium.webdriver.common.by import By
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
 
 
 
 app = Flask(__name__)
 
+selected_products = []
 
 def get_recipe(name):
 
@@ -20,16 +28,10 @@ def get_recipe(name):
 
 	response = requests.get(url, headers=headers, params=querystring)
 
-	print(response.json()[0]['ingredients'])
-	print(response.json()[0]['ingredients'])
-
-	pattern = r"[0-9]|oz|tb|lg|ts|/"
-
 	title = []
 	ingredients = []
 	instructions = []
 	for i in range(0, 3):
-		print(i)
 		title.append(response.json()[i]['title'])
 		ingredients.append(response.json()[i]['ingredients'])
 		instructions.append(response.json()[i]['instructions'])
@@ -43,6 +45,50 @@ def get_recipe(name):
 
 	return recipes
 
+def extract_ingredient_names(recipes):
+	ingredients = recipes[1]
+	list = ','.join(ingredients)
+	pattern = r"[0-9]|oz|tb|lg|ts|/|kg|To|or|and|ml|sm|lb|-|c "
+	ingredient_names_dirty = [re.sub(pattern, '', ingredient).strip() for ingredient in list.split('|')]
+
+	ingredients_names = []
+	for element in ingredient_names_dirty:
+		clean_element = element.split(';')[0]
+		ingredients_names.append((clean_element))
+
+	return ingredients_names
+
+@app.route('/buy_products', methods=['POST'])
+def buy_products():
+    selected_ingredients = request.form.getlist('selected_ingredients')
+
+    # Ici, vous devriez effectuer le scraping en fonction des produits sélectionnés
+    for ingredient in selected_ingredients:
+        driver = webdriver.Firefox()
+        driver.get('https://www.carrefour.fr/')
+
+        wait = WebDriverWait(driver, 10)
+        cookie = wait.until(
+            EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Continuer sans accepter')]")))
+        cookie.click()
+
+        input_search = driver.find_element(By.XPATH, "//input[contains(@placeholder, 'Pain, lait, oeufs...')]")
+        input_search.send_keys(ingredient)
+
+        search_button = driver.find_element(By.XPATH,
+                                           "//button[contains(@aria-label, 'Rechercher parmi le contenu du site')]")
+        search_button.click()
+
+        product_card = driver.find_element(By.XPATH, "//li[@style='order: 5;']")
+
+        product_info = product_card  # Assurez-vous que product_card contient les informations du produit
+        if product_info:
+            selected_products.append(product_info)
+
+        driver.quit()  # Fermez le navigateur après avoir terminé le scraping
+
+    return render_template('shopping_basket.html', selected_products=selected_products)
+
 
 
 @app.route('/')
@@ -55,6 +101,18 @@ def submit():
     print("Name submitted:", name)
     recipes = get_recipe(name)
     return render_template('interface.html', recipes=recipes)
+
+@app.route('/recipe')
+def display_products():
+    name = request.args.get('name')
+    recipes = get_recipe(name)
+    ingredients_names = extract_ingredient_names(recipes)
+    return render_template('recipe.html', ingredients_names=ingredients_names)
+
+
+
+
+
 
 if __name__ == '__main__':
     app.run(port=5009)
--- projet.py
+++ projet.py
@@ -7,74 +7,16 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 
-url = "https://recipe-by-api-ninjas.p.rapidapi.com/v1/recipe"
 
-querystring = {"query": "pizza"}
+list = ['2 tb Butter or oil|2 Big onions; peeled and sliced|2 Big cloves garlic; peeled and crushed|1 kg To 1.25kg chuck or bladebone steak|2 tb Plain flour|1 tb Dry mustard|1 sm Can; (370ml) beer|2 tb Brown vinegar|1 Heaped teaspoon brown sugar|1/2 ts Salt|Bouquet garni; (parsley, thyme and bay leaf tied to a piece of celery with white cotton or string)|Chopped fresh parsley', '1/4 lb To 1/2 lb bacon or pancetta|1 c Milk|2 tb Vinegar|2 Eggs or 1-4 oz. carton egg substitute|1/4 c Grated parmesan', '1 lb Bacon; chopped in 1" pieces|8 tb Sweet butter|1 Medium onion; chopped|1 c Light cream (half-n-half)|1 Egg; lightly beaten|1 c Parmesan cheese; grated|1/2 c Italian parsley; chopped|1 lb Spaghetti or linguine|3/4 c Chicken broth; heated|Pepper; freshly ground']
+list = ','.join(list)
+pattern = r"[0-9]|oz|tb|lg|ts|/|kg|To|or|and|ml|sm|lb|-|c "
+ingredient_names_dirty = [re.sub(pattern, '', ingredient).strip() for ingredient in list.split('|')]
 
-headers = {
-	"X-RapidAPI-Key": "c2eea8a81fmsha2cba9d9c3840f2p195d04jsn76a0cf0025dd",
-	"X-RapidAPI-Host": "recipe-by-api-ninjas.p.rapidapi.com"
-}
-
-response = requests.get(url, headers=headers, params=querystring)
-
-print(response.json())
-
-title = []
-ingredients = []
-instructions = []
-
-for i in range(0, 3):
-	print(i)
-	title.append(response.json()[i]['title'])
-	ingredients.append(response.json()[i]['ingredients'])
-	instructions.append(response.json()[i]['instructions'])
+ingredients_names=[]
+for element in ingredient_names_dirty:
+	clean_element = element.split(';')[0]
+	ingredients_names.append((clean_element))
 
 
-	recipes = list(zip(title, ingredients, instructions))
-
-print('voici les tires', title)
-print('voici les ingrédients', ingredients)
-print('voici les instructions', instructions)
-print(recipes[0])
-
-import re
-
-def extract_ingredient_names(ingredients):
-    # Utilisation d'une expression régulière pour extraire les noms d'ingrédients
-    pattern = r"[0-9]|oz|tb|lg|ts|/"
-    ingredient_names = [re.sub(pattern, '', ingredient).strip() for ingredient in ingredients.split('|')]
-    return ingredient_names
-
-ingredients = '4 oz Prunes|4 oz Dried apricots|1/2 c Toasted split almonds|3 tb Extra virgin olive oil|8 Portions chicken|1 ts Salt|20 Grinds black pepper|1 lg Onion|2 Cloves garlic|1 ts Ground turmeric|3 Cardamom pods|1 ts Ground ginger|2 ts Ground cinnamon|2 1/2 c Chicken stock (or half stock, half white wine)|1 tb Honey|2 ts Cornstarch|2 ts Lemon juice or water'
-
-ingredient_names = extract_ingredient_names(ingredients)
-
-print(ingredient_names)
-
-
-def scrap_walmart():
-
-	driver = webdriver.Firefox()
-	driver.get('https://www.carrefour.fr/')
-
-	wait = WebDriverWait(driver, 10)
-	cookie = wait.until(
-		EC.presence_of_element_located((By.XPATH, "//button[contains(text(), 'Continuer sans accepter')]")))
-	cookie.click()
-
-	input_search = driver.find_element(By.XPATH, "//input[contains(@placeholder, 'Pain, lait, oeufs...')]")
-	input_search.send_keys('chicken')
-
-	search_button = driver.find_element(By.XPATH, "//button[contains(@aria-label, 'Rechercher parmi le contenu du site')]")
-	search_button.click()
-
-	product_card = driver.find_element(By.XPATH, "//li[@style='order: 5;']")
-
-
-
-
-
-
-scrap_walmart()
-
+print(ingredients_names)
